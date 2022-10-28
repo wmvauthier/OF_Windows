e5d94a154aeb5f423d28ecd71859703a09f11f57
C1321100 Wesley Matheus Vauthier <c1321100@interno.bb.com.br>
2022-10-28 16:59:18 -0300

    Detecção de Python

--- modules/shellCommands.js
+++ modules/shellCommands.js
@@ -11,7 +11,7 @@ module.exports.cdAndLs = `cd ${directory} && dir /B`;
 module.exports.getGitCommits = `git log --name-status --no-merges --perl-regexp --author="${yourKey.toLowerCase()}|${yourKey.toUpperCase()}" --after=${choosenDate} --before=${otherDate} --all --pretty=format:"commit: #%h"`;
 module.exports.generateGitCommits = (projectName) => { return `cd ${directory}\\${projectName} && ${this.getGitCommits}`; }
 
-module.exports.getGitCommitsOutput = `git log --name-status --no-merges --perl-regexp --author="${yourKey.toLowerCase()}|${yourKey.toUpperCase()}" --after=${choosenDate} --before=${otherDate} --all --pretty=format:"commit: #%h" > ${directoryOF}/input.txt`;
+module.exports.getGitCommitsOutput = `git log --name-status --no-merges --perl-regexp --author="${yourKey.toLowerCase()}|${yourKey.toUpperCase()}" --after=${choosenDate} --before=${otherDate} --all --pretty=format:"commit: #%h" > ${directoryOF}\\input.txt`;
 module.exports.generateGitCommitsOutput = (projectName) => { return `cd ${directory}\\${projectName} && ${this.getGitCommitsOutput}`; }
 
 module.exports.getFullReportGit = (projectName) => { return `cd ${directory}\\${projectName} && git log --no-merges --graph --stat --perl-regexp --author="${yourKey.toLowerCase()}|${yourKey.toUpperCase()}" --after=${choosenDate} --before=${otherDate} --all --pretty=format:"%as - #%h - %s %cn"`; }
