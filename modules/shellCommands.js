c6a25ae16afc2cdeaa2f30895563e0815e72bb5a
C1321100 Wesley Matheus Vauthier <c1321100@interno.bb.com.br>
2023-12-11 17:03:50 -0300

    Commit Inicial

--- modules/shellCommands.js
+++ modules/shellCommands.js
@@ -8,10 +8,10 @@ let choosenDate = user.choosenDate;
 let otherDate = user.otherDate;
 
 module.exports.cdAndLs = `cd ${directory} && dir /B`;
-module.exports.getGitCommits = `git log --name-status --no-merges --perl-regexp --author="${yourKey.toLowerCase()}|${yourKey.toUpperCase()}" --after=${choosenDate} --before=${otherDate} --all --pretty=format:"commit: #%h"`;
+module.exports.getGitCommits = `git log --name-status --no-merges --perl-regexp --author="${yourKey.toLowerCase()}|${yourKey.toUpperCase()}" --after=${choosenDate} --before=${otherDate} --grep="[^\s]*" --all --pretty=format:"commit: #%h"`;
 module.exports.generateGitCommits = (projectName) => { return `cd ${directory}\\${projectName} && ${this.getGitCommits}`; }
 
-module.exports.getGitCommitsOutput = `git log --name-status --no-merges --perl-regexp --author="${yourKey.toLowerCase()}|${yourKey.toUpperCase()}" --after=${choosenDate} --before=${otherDate} --all --pretty=format:"commit: #%h" > ${directoryOF}\\input.txt`;
+module.exports.getGitCommitsOutput = `git log --name-status --no-merges --perl-regexp --author="${yourKey.toLowerCase()}|${yourKey.toUpperCase()}" --after=${choosenDate} --before=${otherDate} --grep="[^\s]*" --all --pretty=format:"commit: #%h" > ${directoryOF}\\input.txt`;
 module.exports.generateGitCommitsOutput = (projectName) => { return `cd ${directory}\\${projectName} && ${this.getGitCommitsOutput}`; }
 
-module.exports.getFullReportGit = (projectName) => { return `cd ${directory}\\${projectName} && git log --no-merges --graph --stat --perl-regexp --author="${yourKey.toLowerCase()}|${yourKey.toUpperCase()}" --after=${choosenDate} --before=${otherDate} --all --pretty=format:"%as - #%h - %s %cn"`; }
+module.exports.getFullReportGit = (projectName) => { return `cd ${directory}\\${projectName} && git log --no-merges --graph --stat --perl-regexp --author="${yourKey.toLowerCase()}|${yourKey.toUpperCase()}" --after=${choosenDate} --before=${otherDate} --grep="[^\s]*" --all --pretty=format:"%as - #%h - %s %cn"`; }
